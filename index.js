fcda7667c67ab37ea6591ce8020198c8bef8ad9c
rafi993 <rafi993@protonmail.com>
2019-12-04 16:04:51 +0530

    Search overflow

---
+++ index.js
@@ -0,0 +1,35 @@
+const stackoverflow = Array.from(document.querySelectorAll(".r > a"))
+  .map(a => a.href)
+  .filter(link => link.includes("stackoverflow.com"));
+
+const getAnswer = html => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(html, "text/html");
+  const acceptedAnswer = doc.querySelector(".accepted-answer");
+  const answers = doc.querySelector(".answer");
+  if (acceptedAnswer) {
+    return doc.querySelector(".accepted-answer");
+  } else if (answers) {
+    return doc.querySelector(".answer");
+  }
+};
+
+if (stackoverflow.length > 0) {
+  const firstLink = stackoverflow[0];
+  fetch(`https://test.cors.workers.dev/?${firstLink}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "text/plain"
+    }
+  })
+    .then(response => response.text())
+    .then(html => {
+      const answer = getAnswer(html);
+      // console.log(anwser);
+      const appBar = document.getElementById("appbar");
+      appBar.appendChild(answer);
+    })
+    .catch(error => {
+      console.log("Unable to fetch anwser", error);
+    });
+}
